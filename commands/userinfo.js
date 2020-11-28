10c21dce675bb92dd1c92dbbf5edd7edfb0600ba
Nolan <ncanfield27@gmail.com>
2020-11-28 17:22:52 -0500

    thirty-sixth commit

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -2,46 +2,24 @@ module.exports = {
     name: 'userinfo',
     description: "Shows the authors info.",
     execute(message, args, Discord, botversion){
-        run = async (message) => {
-            const { guild, channel } = message
-        
-            const user = message.mentions.users.first() || message.member.user
-            const member = guild.members.cache.get(user.id)
-        
-            console.log(member)
-        
-            const userinfoEmbed = new Discord.MessageEmbed()
-              .setAuthor(`User info for ${user.username}`, user.displayAvatarURL())
-              .addFields(
-                {
-                  name: 'User tag',
-                  value: user.tag,
-                },
-                {
-                  name: 'Is bot',
-                  value: user.bot,
-                },
-                {
-                  name: 'Nickname',
-                  value: member.nickname || 'None',
-                },
-                {
-                  name: 'Joined Server',
-                  value: new Date(member.joinedTimestamp).toLocaleDateString(),
-                },
-                {
-                  name: 'Joined Discord',
-                  value: new Date(user.createdTimestamp).toLocaleDateString(),
-                },
-                {
-                  name: 'Roles',
-                  value: member.roles.cache.size - 1,
-                }
-              )
-              .setFooter('Version: ' + botversion)
-              .setTimestamp()
-        
-            message.channel.send(userinfoEmbed);
-          }
-        }
+        const user = message.mentions.users.first() || message.member.user;
+        const member = guild.members.cache.get(user.id);
+
+        const userinfoEmbed = new Discord.MessageEmbed()
+        .setColor('#6DB6D9')
+        .setTitle("__User Information__")
+        .setDescription("Here is your user info.")
+        .addFields(
+            {name: 'Username', vale: user.tag},
+            {name: 'Is Bot', vale: user.bot},
+            {name: 'Joined Server', vale: new Date(member.joinedTimestamp).toLocaleDateString()},
+            {name: 'Joined Discord', vale: new Date(user.createdTimestamp).toLocaleDateString()},
+            {name: 'Roles', vale: member.roles.cache.size - 1}
+        )
+        .setThumbnail(user.displayavatarUrl())
+        .setFooter('Version: ' + botversion)
+        .setTimestamp()
+
+        message.channel.send(userinfoEmbed)
     }
+}
