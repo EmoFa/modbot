b69d06a1bd595dc681a207b01c4257c6efc348bf
Nolan <ncanfield27@gmail.com>
2020-11-27 23:46:54 -0500

    eleventh commit

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,7 @@ module.exports = {
         const newEmbed = new Discord.MessageEmbed()
         .setColor('#6DB6D9')
         .setTitle('__Moderation__')
-        .setDescription('Here are the current moderation commands for the bot.')
+        .setDescription('Here are the current moderation commands for this bot.')
         .addFields(
             {name: 'kick', value: "You can use this command to kick a member. \nExample: -kick @Username#1234"},
             {name: 'ban', value: "You can use this command to ban a member. \nExample: -ban @Username#1234"},
@@ -14,7 +14,18 @@ module.exports = {
         )
         .setImage('https://i.imgur.com/O3DHIA5.gif')
 
+        const newEmbed2 = new Discord.MessageEmbed()
+        .setColor('#6DB6D9')
+        .setTitle('__Utility__')
+        setDescription('Here are the current utility commands for this bot.')
+        .addFields(
+            {name: 'clear', value: "You can use this command to clear a certain amount of messages. (including the command you sent) \nExample: -clear 10"}
+        )
+        .setImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcHkaDgLOYmj0KsOTvseK_pYbn4dXnvXmuph13aLzadJ0sMfYbzjkYqNThk-vkB69nGlSU_HQngg&usqp=CAc')
+
         message.author.send(newEmbed);
+        message.author.send(newEmbed2);
+        message.react('✅');
     }
 }
 
