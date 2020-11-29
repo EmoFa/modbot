5724d6162d599dcaaa1f34bf50139211cf6e89dc
Nolan <ncanfield27@gmail.com>
2020-11-28 22:30:44 -0500

    fiftieth commit

--- commands/ping.js
+++ commands/ping.js
@@ -2,10 +2,6 @@ module.exports = {
     name: 'ping',
     description: 'The bot responds with pong.',
     execute(message, args){
-        client.on('message', message => {
-            if (message.content === '+ping') {  
-              message.channel.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
-            }
-          });
+        message.channel.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
     }
 }
