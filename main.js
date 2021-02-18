4e5f3d1fc880e20380bd1ba9a25dcd0e9635583a
Nolan <ncanfield27@gmail.com>
2021-02-18 16:29:16 -0500

    one hundred seventeenth commit

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 //const config = require('./config.json');
 const client = new Discord.Client();
+const mongoose = new require('mongoose');
 
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
@@ -13,4 +14,15 @@ client.once('ready', () =>{
     client.user.setActivity('for -', {type: "WATCHING"}).catch(console.error);
 });
 
+mongoose.connect(process.env.MONOGDB_SRV, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}).then(() => {
+    console.log('Connected to the database.');
+}).catch((err) => {
+    console.log(err);
+});
+
 client.login(process.env.DJS_TOKEN);
+//DiscordBotDB
